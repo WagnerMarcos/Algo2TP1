139db46653cd4beffcc746775647359c9f30373e
tmgrassi <tomas96@gmail.com>
2018-05-14 18:07:15 -0300

    Cambios a fourier_test.cpp

--- include/fourier_test.h
+++ include/fourier_test.h
@@ -1,9 +1,9 @@
-#ifndef _DFT_TEST_H_INCLUDED_
-#define _DFT_TEST_H_INCLUDED_
+#ifndef _FOURIER_TEST_H_INCLUDED_
+#define _FOURIER_TEST_H_INCLUDED_
 
 #include "cmdline.h"
 #include "io.h"
-#include "DFT.h"
+#include "fourier.h"
 #include "Complex.h"
 #include "Vector.h"
 
@@ -12,4 +12,4 @@
 static void opt_number(std::string const &arg);
 static void opt_help(std::string const &arg);
 
-#endif // _DFT_TEST_H_INCLUDED_
+#endif // _FOURIER_TEST_H_INCLUDED_
